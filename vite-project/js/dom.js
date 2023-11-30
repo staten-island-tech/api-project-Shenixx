3c065605ba0b8d6f1309f0bac353017ee0c72a4c
Shenixx <113193025+Shenixx@users.noreply.github.com>
2023-11-30 09:54:48 -0500

    asdsadsadas

---
+++ vite-project/js/dom.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+    box: document.getElementById("app"),
+    button: document.querySelectorAll("#btn"),
+    themeSwitcher: document.getElementById("themeSwitcher"),
+};  
+
+export {DOMSelectors};
